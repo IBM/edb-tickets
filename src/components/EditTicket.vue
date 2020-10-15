c5f454e692d26e50fa27c07a98ce98a4cdec42c9
Rich Hagarty <rich.hagarty@ibm.com>
2020-10-14 18:13:21 -0700

    reusable comp

---
+++ src/components/EditTicket.vue
@@ -0,0 +1,108 @@
+<template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">Edit Ticket</span>
+        </v-card-title>
+
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-text-field
+                v-model="ticket.subject"
+                label="Subject"
+              ></v-text-field>
+            </v-row>
+            <v-row>
+              <v-text-field
+                v-model="ticket.text"
+                label="Details"
+              ></v-text-field>
+            </v-row>
+
+                <!-- :hint="'${selectedName}'" -->
+
+            <v-row>
+              <v-select
+                v-model="selectedName"
+                :items="userNames"
+                label="Assigned to"
+                @change='userSelected'
+              ></v-select>
+            </v-row>
+
+          </v-container>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="$emit('cancel', ticket)"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="$emit('save', ticket)"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'edit-ticket-dialog',
+    props: {
+      editTicket: {},
+      allUsers: { type: Array, required: true },
+      allAssignees: { type: Array, required: true }
+    },
+    data() {
+      return {
+        ticket: this.editTicket,
+        cachedTicket: {},
+        dialogEdit: false,
+        users: this.allUsers,
+        assignees: this.allAssignees,
+        userNames: [],
+        select: { name: 'Martha Pearl Caracal' },
+        selectedName: ''
+      }
+    },
+    mounted() {
+      this.getUserNames()
+    },
+    methods: {
+      getUserNames() {
+        this.allAssignees.forEach((assignee) => {
+          let user = this.allUsers.find(u => u.id === assignee.id);
+          this.userNames.push(user.name);
+          if (user.id === this.editTicket.assignee_id) {
+            this.selectedName = user.name
+          }
+        });
+        console.log('Selected USER: ' + this.selectedName);
+      },
+      userSelected() {
+        this.allUsers.forEach((user) => {
+          if (this.selectedName === user.name) {
+            this.ticket.assignee_id = user.id;
+            console.log('CHANGE ASSIGNEE ID TO: ' + user.id);
+          }
+        });
+      },
+      async assignMe () {
+        let response = await this.addAssignee();
+        const r = await response.json();
+        this.editedItem.assignee_id = r[0].id;
+      },
+    }
+  }
+</script>
+
+<style scoped>
+</style>
