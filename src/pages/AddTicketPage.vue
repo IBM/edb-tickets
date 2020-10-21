1a4be6d5ec884d16eb6141cd540020c6d493c63f
Mark Sturdevant <mark.sturdevant@ibm.com>
2020-10-21 12:38:13 -0700

    Success/error notification on submit ticket
    * alert pops up for success/error
    * can dismiss with the x
    * success dismiss clears the form
    * error dismiss keeps the form values
    * either one clears the validation errors (so they are not all read after success+clear)

--- src/pages/AddTicketPage.vue
+++ src/pages/AddTicketPage.vue
@@ -13,12 +13,13 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
- 
+
 <template>
   <div id="addTicket">
     <v-form
       @submit.prevent="addTicket"
       id="addTicket"
+      ref="form"
     >
 
       <v-text-field
@@ -49,7 +50,25 @@
         label="Detailed description"
         :rules="[v => !!v || 'Description is required']"
         outlined
-      />
+      ></v-textarea>
+
+      <v-alert v-model="errorAlert" type="error" @input="this.closeAlert"
+               transition="scale-transition" outlined dismissible dense text prominent>
+          <v-row dense>
+              <v-col class="text-lg-caption">
+                  {{ errorTitle }}
+              </v-col>
+          </v-row>
+          <v-row dense>
+              <v-col class="text-sm-caption">
+                  {{ errorSubTitle }}
+              </v-col>
+          </v-row>
+      </v-alert>
+      <v-alert v-model="successAlert" type="success" @input="this.doReset"
+               transition="slide-x-reverse-transition" outlined dismissible dense text>
+          Success
+      </v-alert>
 
       <v-btn :disabled="isDisabled" type="submit">submit ticket</v-btn>
     </v-form>
@@ -84,6 +103,11 @@
           'None',
         ],
 
+        successAlert: false,
+        errorAlert: false,
+        errorTitle: '',
+        errorSubTitle: '',
+
       }
     },
     computed: {
@@ -92,7 +116,18 @@
       }
     },
     methods: {
+      closeAlert() {
+        this.successAlert = false;
+        this.errorAlert = false;
+        this.errorTitle = '';
+        this.errorSubTitle = '';
+      },
+      doReset() {
+        this.closeAlert();
+        this.$refs.form.reset()
+      },
       async addTicket() {
+        this.closeAlert();
         try {
           const response = await fetch('/api/v1/tickets', {
             method: 'POST',
@@ -106,10 +141,18 @@
             }),
             headers: { "Content-type": "application/json; charset=UTF-8" }
           });
-          const r = await response.json();
-          console.log("INSERTED:", r)
+          if (response.status >= 200 && response.status < 300) {
+            this.successAlert = true;
+          } else {
+            this.errorAlert = true;
+            this.errorTitle = response.statusText;
+            this.errorSubTitle = `HTTP Status Code: ${response.status}`;
+          }
         } catch (error) {
-          console.error(error)
+          console.error(error);
+          this.errorAlert = true;
+          this.errorTitle = 'ERROR';
+          this.errorSubTitle = error;
         }
       },
     },
