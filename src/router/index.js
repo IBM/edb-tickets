9d7a90d64e79ac5e39118f7786b0dff9f1a6fcaf
Mark Sturdevant <mark.sturdevant@ibm.com>
2020-10-23 21:15:48 -0700

    Error handling and cleanup
    * Notify when edit fails
    * Use forceRerender for clean forms
    * Demo snackbar for success
    * Fix extra card overlap on All Tickets
    * Use outline pencil and trash can
    * Remove unused view
    * Make some of the sample data 'open'

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,6 @@ import VueRouter from 'vue-router'
 import AllTickets from '../pages/AllTicketsPage.vue'
 import HomePage from '../pages/HomePage.vue'
 import AddTicket from '../pages/AddTicketPage.vue'
-import TicketTable from '../pages/TicketTable.vue'
 import Assignees from '../pages/SupportStaffPage.vue'
 
 Vue.use(VueRouter);
@@ -38,12 +37,6 @@ const routes = [
     props: true
   },
   {
-    path: "/mytickets",
-    name: "TicketTable",
-    component: TicketTable,
-    props: true
-  },
-  {
     path: "/alltickets",
     name: "AllTickets",
     component: AllTickets,
@@ -55,11 +48,11 @@ const routes = [
     component: Assignees,
     props: true
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   routes
-})
+});
 
 export default router
